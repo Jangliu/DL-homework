5a75d651e2cdac31d9722c82cbc60c3aec7dba55
Jangliu <709513322@qq.com>
2018-10-21 11:54:00 +0800

    successed

--- Multi-Class.py
+++ Multi-Class.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import math
+# numpy矩阵正常相乘为用dot，点乘用*
 X = np.array([
     [
         0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1,
@@ -24,13 +25,13 @@ X = np.array([
 ])
 D = np.array([[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0],
               [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]])
-W1 = np.random.rand(25, 40)
-W2 = np.random.rand(40, 5)
+W1 = np.random.rand(25, 50)
+W2 = np.random.rand(50, 5)
 
 
 def Sigmoid(x):
     temp = []
-    if type(x) != type(X):
+    if isinstance(x, type(np.mat(X))):
         y = 1 / (1 + np.exp(-x))
     else:
         for element in x.flat:
@@ -46,18 +47,12 @@ def Sigmoid(x):
 
 def Softmax(x):
     sum_ex = 0
-    temp = []
+    y = []
     for element in x.flat:
-        if element >= 700:
-            y = np.zeros(1, 5)
-            return y
-        elif element <= -700:
-            sum_ex += 0
-        else:
-            sum_ex += np.exp(element)
-    for i in range(x.size):
-        temp.append(np.exp(x[i]) / sum_ex) 
-    y = np.array(temp)
+        sum_ex += math.exp(element)
+    for element in x.flat:
+        y.append(math.exp(element)/sum_ex)
+    y = np.array(y)
     return y
 
 
@@ -66,21 +61,19 @@ def Multi_Class(X, D):
     global W2
     alpha = 0.9
     for i in range(0, 5):
-        x = X[i]
+        x = X[i].reshape(1, 25)
         d = D[i]
         v1 = np.dot(x, W1)  # v1 1x50的矩阵
         y1 = Sigmoid(v1)  # y1 1x50的矩阵
         v2 = np.dot(y1, W2)  # V2 1x5矩阵
-        y2 = Sigmoid(v2)
+        y2 = Softmax(v2)
         e2 = d - y2
         delta2 = e2
-        e1 = np.dot(W2, delta2)  # e1 1x50的矩阵
-        delta1 = y1 * (1 - y1) * e1  # delta1 1x50矩阵
-        dW1 = alpha * np.reshape(x, (25, 1)) * np.reshape(delta1, (1, 40))
+        e1 = np.dot(delta2, W2.T)  # e1 1x50的矩阵
+        delta1 = y1*(1-y1)*e1  # delta1 1x50矩阵
+        dW1 = alpha * np.dot(x.T, delta1.reshape(1, 50))
         W1 = W1 + dW1
-        y1 = np.reshape(40, 1)
-        delta2 = delta2.T
-        dW2 = alpha * y1 * delta2
+        dW2 = alpha * np.dot(y1.reshape(50, 1), delta2.reshape(1, 5))
         W2 = W2 + dW2
 
 
